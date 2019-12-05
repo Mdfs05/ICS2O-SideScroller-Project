b900c83be81e8c7c4c4294420d3451f7213a221e
Mdfs05 <55096826+Mdfs05@users.noreply.github.com>
2019-12-04 19:44:03 -0500

    person.js
    The person.js file

--- person.js
+++ person.js
@@ -1,24 +1,50 @@
-function Person() {
-  this.pos = createVector(50, height-50);
-  this.vel = createVector(1, 0);
-  this.acc = createVector(0, -0);
+
+
+function Person(x, y) {
+  this.pos = createVector(50, height);
+  this.vel = createVector(1.5, 0);
+  this.acc = createVector(0, 0);
+  this.score = 0;
   // this.mass = 0;
-  
+
   this.applyForce = function(force) {
     // var f = force.copy();
     // f.div(this.mass);
     this.acc.add(force);
   }
-  
+
   this.update = function() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
-  
+
   this.display = function() {
     fill(255);
-    rect(this.pos.x, this.pos.y, 30, 50);
+    stroke(255);
+    rect(this.pos.x, this.pos.y - 50, 30, 50, 30);
   }
-  
+
+  this.edges = function() {
+    if (this.pos.y > height) {
+      this.vel.y *= -0.5; //-1 for bouncing
+      this.pos.y = height;
+    }
+
+
+    // if(this.pos.x > width) {
+    //   this.vel.x *= -1;
+    //   this.pos.x = width;
+    // }
+  }
+
+  this.hits = function(coin) {
+    //if person is over coin, do something
+    if (this.pos.x >= coin.pos.x && this.pos.x <= coin.pos.x + 40 && this.pos.y >= coin.pos.y && this.pos.y <= coin.pos.y + 100) // 100? See if 40
+    {
+      coin.pos.y = -400;
+      this.score++;
+    }
+  }
+
 }
